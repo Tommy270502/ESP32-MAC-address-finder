873caf6f50f729bcc24c31073508f527e393cca8
Thomas S. Perri <87670767+Tommy270502@users.noreply.github.com>
2021-12-16 14:56:31 +0100

    Update mac_adress_finder.ino

--- mac_adress_finder/mac_adress_finder.ino
+++ mac_adress_finder/mac_adress_finder.ino
@@ -1,3 +1,16 @@
+/*********************************************************************************\
+* Filename: mac_address_finder.ino
+* Projekt  : MAC address finder for the ESP32
+* Hardware : Espressif Systems ESP32
+*
+* Description:
+* This code reads out the MAC address of the ESP32
+* and writes it to the serial monitor.
+*
+* Date:       Author:           Version
+* 16.12.2021  Thomas S. Perri   V1.0
+\*********************************************************************************/
+
 #include "WiFi.h"
  
 void setup(){
